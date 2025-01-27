05f409685ad0dddbfedecb58a7768e7b46277b95
duelHunter <99xdilshan@gmail.com>
2025-01-27 17:29:02 +0530

    basic map

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import MapTest from "../components/MapTest";
+
 
 export default function Home() {
   return (
     <div>
       <h1>Dashboard</h1>
+      <MapTest />
       
     </div>
   );
 }
+  //fff
---
+++ dashboard/src/components/MapTest.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Correctly configure the default icon
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+  });
+
+  //7.471292, 80.041397
+const MapTest = () => {
+  const position: [number, number] = [7.471292, 80.041397]; // Default center position
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapTest;
