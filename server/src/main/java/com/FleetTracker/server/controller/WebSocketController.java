6fa5e9be015b1227296bf58775be8a1e3c4d9e94
duelHunter <99xdilshan@gmail.com>
2025-01-29 22:37:33 +0530

    spring web socket

---
+++ server/src/main/java/com/FleetTracker/server/controller/WebSocketController.java
@@ -0,0 +1,42 @@
+package com.FleetTracker.server.controller;
+
+import com.FleetTracker.server.handler.WebSocketHandler;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class WebSocketController {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Autowired
+    public WebSocketController(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMessage(@RequestParam String message) {
+        List<WebSocketSession> sessions = webSocketHandler.getSessions();
+        if (sessions.isEmpty()) {
+            return ResponseEntity.badRequest().body("No active WebSocket connections.");
+        }
+
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                try {
+                    session.sendMessage(new TextMessage("Server: " + message));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return ResponseEntity.ok("Message sent to all clients!");
+    }
+}
---
+++ server/src/main/java/com/FleetTracker/server/handler/WebSocketHandler.java
@@ -0,0 +1,47 @@
+package com.FleetTracker.server.handler;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+        logger.info("WebSocket connection established: " + session.getId());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        logger.info("Received message: " + message.getPayload());
+
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(new TextMessage("Server: " + message.getPayload()));
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+        logger.info("WebSocket connection closed: " + session.getId());
+    }
+
+    public List<WebSocketSession> getSessions() {
+        return sessions;
+    }
+}
