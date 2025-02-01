2b7ed8b3a65c0c7e82111bf3fe67a1ad05d90e67
duelHunter <99xdilshan@gmail.com>
2025-02-01 21:28:46 +0530

    new

--- mobile/lib/services/websocket_service.dart
+++ mobile/lib/services/websocket_service.dart
@@ -20,21 +20,45 @@
 // }
 
 
-import 'package:web_socket_channel/io.dart';
+// import 'package:web_socket_channel/io.dart';
+// import 'package:web_socket_channel/web_socket_channel.dart';
+
+// class WebSocketService {
+//   final String url;
+//   late WebSocketChannel _channel;
+
+//   WebSocketService({required this.url}) {
+//     _channel = IOWebSocketChannel.connect(url);
+//   }
+
+//   Stream get stream => _channel.stream;
+
+//   void sendMessage(String message) {
+//     _channel.sink.add(message);
+//   }
+
+//   void closeConnection() {
+//     _channel.sink.close();
+//   }
+// }
+
+
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WebSocketService {
   final String url;
   late WebSocketChannel _channel;
-
+  
   WebSocketService({required this.url}) {
-    _channel = IOWebSocketChannel.connect(url);
+    _channel = WebSocketChannel.connect(Uri.parse(url));
   }
 
   Stream get stream => _channel.stream;
 
   void sendMessage(String message) {
-    _channel.sink.add(message);
+    if (_channel != null && _channel.sink != null) {
+      _channel.sink.add(message);
+    }
   }
 
   void closeConnection() {
